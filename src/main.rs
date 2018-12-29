03ed9f89a83e63d5f43ac79dad4f7d31efd7f3ef
Guenther <guenther.stangassinger@gmx.de>
2018-12-29 18:32:20 +0000

    ...

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,38 @@ use board::hal::time::*;
 
 use cortex_m::peripheral::Peripherals;
 
+
+// Display
+/// Width and height of visible screen.
+const WIDTH: u16 = 480;
+const HEIGHT: u16 = 128;
+
+
+/// Horizontal display timing.
+const H_SYNCPULSE:  u16 = 11;
+const H_BACKPORCH:  u16 = 5;
+const H_ACTIVE:     u16 = WIDTH;
+const H_FRONTPORCH: u16 = 28;
+
+/// Vertical display timing.
+const V_SYNCPULSE:  u16 = 2;
+const V_BACKPORCH:  u16 = 3;
+const V_ACTIVE:     u16 = 272;  // different from HEIGHT!
+const V_FRONTPORCH: u16 = 8;
+
+/// Upper-left corner of screen for layer windows.
+const H_WIN_START:  u16 = H_SYNCPULSE + H_BACKPORCH - 1;
+const V_WIN_START:  u16 = V_SYNCPULSE + V_BACKPORCH - 1;
+
+// Graphics framebuffer
+const FB_GRAPHICS_SIZE: usize = (WIDTH as usize) * (HEIGHT as usize);
+
+#[link_section = ".sram1bss"]
+static mut FB_GRAPHICS: [u8; FB_GRAPHICS_SIZE] = [0; FB_GRAPHICS_SIZE];
+
+
+
+
 #[entry]
 fn main() -> ! {
     if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
