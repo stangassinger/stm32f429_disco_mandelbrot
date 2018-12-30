d3dfd284202112f83ae85648fbfd159393eec330
Guenther <guenther.stangassinger@gmx.de>
2018-12-30 22:17:53 +0000

    not working :-(

--- src/main.rs
+++ src/main.rs
@@ -25,21 +25,21 @@ use cortex_m::peripheral::Peripherals;
 
 // Display
 /// Width and height of visible screen.
-const WIDTH: u16 = 480;
-const HEIGHT: u16 = 128;
+const WIDTH: u16 = 240;
+const HEIGHT: u16 = 320;
 
 
 /// Horizontal display timing.
-const H_SYNCPULSE:  u16 = 11;
-const H_BACKPORCH:  u16 = 5;
+const H_SYNCPULSE:  u16 = 10;
+const H_BACKPORCH:  u16 = 20;
 const H_ACTIVE:     u16 = WIDTH;
-const H_FRONTPORCH: u16 = 28;
+const H_FRONTPORCH: u16 = 10;
 
 /// Vertical display timing.
 const V_SYNCPULSE:  u16 = 2;
-const V_BACKPORCH:  u16 = 3;
-const V_ACTIVE:     u16 = 272;  // different from HEIGHT!
-const V_FRONTPORCH: u16 = 8;
+const V_BACKPORCH:  u16 = 2;
+const V_ACTIVE:     u16 = HEIGHT;  
+const V_FRONTPORCH: u16 = 4;
 
 /// Upper-left corner of screen for layer windows.
 const H_WIN_START:  u16 = H_SYNCPULSE + H_BACKPORCH - 1;
@@ -57,11 +57,6 @@ static mut FB_GRAPHICS: [u8; FB_GRAPHICS_SIZE] = [0; FB_GRAPHICS_SIZE];
 #[entry]
 fn main() -> ! {
     if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
-        let gpiod = p.GPIOG.split();
-
-        // (Re-)configure PG13 (green LED) as output
-        let mut led = gpiod.pg14.into_push_pull_output();
-
         // Constrain clock registers
         let mut rcc = p.RCC.constrain();
 
@@ -77,19 +72,24 @@ fn main() -> ! {
     let gpiob = p.GPIOB.split();
     let gpioc = p.GPIOC.split();
     let gpiod = p.GPIOD.split();
-    let gpioe = p.GPIOE.split();    
+    let gpioe = p.GPIOE.split();   
+    let gpiof = p.GPIOF.split();   
+    let gpiog = p.GPIOG.split();
+
+    // (Re-)configure PG13 (green LED) as output
+    let mut led = gpiog.pg14.into_push_pull_output(); 
 
     // LCD enable: set it low first to avoid LCD bleed fl setting up timings
-    let mut disp_on = gpioa.pa8.into_push_pull_output();
-    disp_on.set_low();
+ //   let mut disp_on = gpioa.pa8.into_push_pull_output();
+ //   disp_on.set_low();
 
     // LCD backlight enable
-    let mut backlight = gpiod.pd12.into_push_pull_output();
-    backlight.set_high();
+ //   let mut backlight = gpiod.pd12.into_push_pull_output();
+ //   backlight.set_high();
 
     // Output pin connected to Boot0 + capacitor
-    let mut bootpin = gpiob.pb7.into_push_pull_output();
-    bootpin.set_low();
+ //   let mut bootpin = gpiob.pb7.into_push_pull_output();
+  //  bootpin.set_low();
 
     // LCD pins
     gpioa.pa3 .into_alternate_af14().set_speed(Speed::VeryHigh);
@@ -99,6 +99,8 @@ fn main() -> ! {
     gpioa.pa12.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpiob.pb0 .into_alternate_af9() .set_speed(Speed::VeryHigh);
     gpiob.pb1 .into_alternate_af9() .set_speed(Speed::VeryHigh);
+    gpiog.pg10.into_alternate_af9().set_speed(Speed::VeryHigh);
+    gpiog.pg12.into_alternate_af9().set_speed(Speed::VeryHigh);
     gpiob.pb8 .into_alternate_af14().set_speed(Speed::VeryHigh);
     gpiob.pb9 .into_alternate_af14().set_speed(Speed::VeryHigh);
     gpiob.pb10.into_alternate_af14().set_speed(Speed::VeryHigh);
@@ -108,12 +110,17 @@ fn main() -> ! {
     gpioc.pc10.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpiod.pd3 .into_alternate_af14().set_speed(Speed::VeryHigh);
     gpiod.pd6 .into_alternate_af14().set_speed(Speed::VeryHigh);
-    gpiod.pd10.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpioe.pe11.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpioe.pe12.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpioe.pe13.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpioe.pe14.into_alternate_af14().set_speed(Speed::VeryHigh);
     gpioe.pe15.into_alternate_af14().set_speed(Speed::VeryHigh);
+    gpiof.pf10.into_alternate_af14().set_speed(Speed::VeryHigh);
+    gpiog.pg6.into_alternate_af14().set_speed(Speed::VeryHigh);
+    gpiog.pg7.into_alternate_af14().set_speed(Speed::VeryHigh);
+    gpiog.pg11.into_alternate_af14().set_speed(Speed::VeryHigh);
+    // until here ok with
+    //http://www.lucadavidian.com/2017/10/02/stm32-using-the-ltdc-display-controller/
 
 
 
