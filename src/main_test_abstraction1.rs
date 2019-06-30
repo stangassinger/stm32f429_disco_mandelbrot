0340fc5ad2997fd3085897dcced4ab2f991ccd61
Guenther <guenther.stangassinger@gmx.de>
2019-06-30 20:21:36 +0000

    ...

--- src/main_test_abstraction1.rs
+++ src/main_test_abstraction1.rs
@@ -14,12 +14,13 @@ fn main() -> ! {
 
 
     unsafe {
-        const RCC_AHBENR: u32 = 0x40021000 + 0x14;
-        const GPIOE_BSRR: u32 = 0x48001018;
-        const GPIOE_MODER: u32 = 0x48001000;
+        const RCC_AHB1ENR: u32  = 0x40023800 + 0x30;
+        const GPIOG_BSRR:  u32  = 0x48001018;
+        const GPIOG_MODER: u32  = 0x48001000;
  
-        let x = ptr::read_volatile(RCC_AHBENR as *mut u32);      
-        ptr::write_volatile(RCC_AHBENR as *mut u32, x | (1 << 21));       
+        //using PG13 PG14 as LED
+        let x = ptr::read_volatile( RCC_AHB1ENR as *mut u32 );      
+        ptr::write_volatile(RCC_AHB1ENR as *mut u32, x | (1 << 6));       
         ptr::write_volatile(GPIOE_MODER as *mut u32, (1 << 18) | (1 << 22));    
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
