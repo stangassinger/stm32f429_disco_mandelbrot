16659c8c4e404a449156720b557a9d1f0359dade
guenther stangassinger <guenther.stangassinger@gmx.de>
2019-07-07 15:55:46 +0000

    abstraction level 1 working :-)

--- src/main_test_abstraction1.rs
+++ src/main_test_abstraction1.rs
@@ -1,46 +1,42 @@
+#![no_main]
 #![no_std]
-#![feature(used)]
-#![feature(lang_items)]
 
 extern crate cortex_m;
 extern crate cortex_m_rt;
+extern crate panic_halt;
 
-use cortex_m::asm;
 use core::ptr;
 
-
-#[entry]
+// working :-) just copy to main.rs and run 
+#[ cortex_m_rt::entry ] 
 fn main() -> ! {
 
 
     unsafe {
-        const RCC_AHB1ENR: u32  = 0x40023800 + 0x30;
-        const GPIOG_BSRR:  u32  = 0x48001018;
-        const GPIOG_MODER: u32  = 0x48001000;
+        const RCC_AHB1ENR: u32  = 0x40023800 + 0x30;        
+        const GPIOG_MODER: u32  = 0x40021800;
+        const GPIOG_BSRR:  u32  = 0x40021818;
  
         //using PG13 PG14 as LED
         let x = ptr::read_volatile( RCC_AHB1ENR as *mut u32 );      
-        ptr::write_volatile(RCC_AHB1ENR as *mut u32, x | (1 << 6));       
-        ptr::write_volatile(GPIOE_MODER as *mut u32, (1 << 18) | (1 << 22));    
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
-    }
+        ptr::write_volatile( RCC_AHB1ENR as *mut u32, x | ( 1 << 6 ) );       
+        ptr::write_volatile( GPIOG_MODER as *mut u32, ( 1 << 26) | ( 1 << 28 ));    
+        ptr::write_volatile( GPIOG_BSRR  as *mut u32,   1 << 13 );
+        ptr::write_volatile( GPIOG_BSRR  as *mut u32,   1 << 14 );
+    } 
 
     loop {}
 }
 
 
 
-#[link_section = ".vector_table.interrupts"]
-#[used]
-static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
 
-extern "C"  fn default_handler() {
-    asm::bkpt();
+#[cortex_m_rt::exception]
+fn HardFault(ef: &cortex_m_rt::ExceptionFrame) -> ! {
+    panic!("HardFault at {:#?}", ef);
 }
 
-#[lang = "panic_fmt"]
-#[no_mangle]
-pub extern "C" fn panic_fmt() -> ! {
-    loop {}
+#[cortex_m_rt::exception]
+fn DefaultHandler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
 }
