65372a13eba0430ee79f9f3d9256e99fef50daec
Guenther <guenther.stangassinger@gmx.de>
2019-07-14 16:07:45 +0000

    ---

--- src/main_test_abstraction2.rs
+++ src/main_test_abstraction2.rs
@@ -1,22 +1,23 @@
 #![no_main]
 #![no_std]
 
-extern crate cortex-m;
-extern crate cortex-m-rt;
-extern crate panic_halt;
+use cortex_m;
+use cortex_m_rt;
+use stm32f4::stm32f429;
+//use panic_halt;
 
 
 // working :-) just copy to main.rs and run 
 #[ cortex_m_rt::entry ] 
 fn main() -> ! {
-   let p = Peripherals::take().unwrap();   
+   let p = cortex_m::Peripherals::take().unwrap();   
 
     let gpiog = p.GPIOG;
     let rcc = p.RCC;
 
     rcc.ahbenr.modify( |r, w| w.iopeen().set_bit() );    
-    gpioe.moder.write( |w| w.moder26().output().moder28().output() );
-    gpioe.bsrr.write( |w| w.bs13().set().bs14().set() );
+    gpiog.moder.write( |w| w.moder26().output().moder28().output() );
+    gpiog.bsrr.write( |w| w.bs13().set().bs14().set() );
     
     loop {}
 }
